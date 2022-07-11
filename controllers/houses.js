5f9cd73974506218363a6a5208b07cb2175032f3
kazomikun <106589367+kazomikun@users.noreply.github.com>
2022-07-11 09:20:46 +0100

    Update houses.js

--- controllers/houses.js
+++ controllers/houses.js
@@ -3,15 +3,36 @@ const express = require('express')
 const router = express.Router()
 
 // Create POST controller
+router.post('/', async (req, res) => {
+  res.send('Hello from auth')
+})
 
 // Create GET controller
 router.get('/', async (req, res) => {
   res.send('Hello from houses')
 })
 
+router.get('/create', async (req, res) => {
+  res.send('Hello from auth')
+})
+
+router.get('/:id', async (req, res) => {
+  res.send('Hello from auth')
+})
+
+router.get('/:id/edit', async (req, res) => {
+  res.send('Hello from auth')
+})
+
 // Create PATCH controller
+router.patch('/:id', async (req, res) => {
+  res.send('Hello from auth')
+})
 
 // Create DELETE controller
+router.delete('/:id', async (req, res) => {
+  res.send('Hello from auth')
+})
 
 // Views
 // Create here a controller that accepts GET requests and renders the "search" page
