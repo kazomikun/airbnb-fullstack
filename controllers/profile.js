474b3175979637ed138073f6dbbc3447749c82a0
kazomikun <106589367+kazomikun@users.noreply.github.com>
2022-07-19 11:12:06 +0100

    Update profile.js

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,24 +1,60 @@
 // Import Packages
 const express = require('express')
 const router = express.Router()
+const Users = require('../models/users')
+const Houses = require('../models/houses')
 
 // Create POST controller
 
 // Create GET controller
 router.get('/', async (req, res) => {
-  if (req.isAuthenticated()) {
-    res.render('houses/profile')
-  } else {
-    res.redirect('/auth/login')
+  try {
+    if (req.isAuthenticated()) {
+      let user = req.user
+
+      let myHouses = await Houses.find({
+        host: req.user._id
+      })
+      console.log(myHouses)
+      console.log('hello')
+      res.render('profile', { user, myHouses })
+    } else {
+      res.redirect('/auth/login')
+    }
+  } catch (err) {
+    next(err)
   }
 })
 
 // Create PATCH controller
-router.patch('/', async (req, res) => {
-  if (req.isAuthenticated()) {
-    res.render('houses/one')
-  } else {
-    res.redirect('/auth/login')
+router.patch('/', async (req, res, next) => {
+  try {
+    // console.log(req.user)
+    // everything inside try
+    if (req.isAuthenticated()) {
+      // let newUser = {
+      //   email: req.user.email,
+      //   password: req.user.password
+      // }
+
+      // you need the new user here
+      let newUser = await Users.findByIdAndUpdate(req.user._id, req.body, {
+        new: true
+      })
+
+      req.login(newUser, err => {
+        if (err) {
+          throw err
+        } else {
+          // console.log('login ok')
+          res.redirect('/profile')
+        }
+      })
+    } else {
+      res.redirect('/auth/login')
+    }
+  } catch (err) {
+    next(err)
   }
 })
 
