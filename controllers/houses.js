fedeb7b639f2bbb6b23d1bf1551e4b600d5cbe2d
kazomikun <106589367+kazomikun@users.noreply.github.com>
2022-07-19 11:12:08 +0100

    Update houses.js

--- controllers/houses.js
+++ controllers/houses.js
@@ -9,11 +9,19 @@ router.post('/', async (req, res, next) => {
     // console.log(req.body)
     // console.log(req.user)
 
+    // console.log(req.body.photos)
+    let filteredPhotos = req.body.photos.filter(n => n != '')
+    req.body.photos = filteredPhotos
+    // console.log(req.body.photos)
+
+    //console.log(req.body)
+
     req.body.host = req.user._id
     // console.log(req.body)
+
     let house = await Houses.create(req.body)
-    console.log(house._id)
-    res.redirect('houses/id=' + house._id)
+    // console.log(house._id)
+    res.redirect('houses/' + house._id)
   } else {
     res.redirect('/auth/login')
   }
@@ -22,9 +30,37 @@ router.post('/', async (req, res, next) => {
 // Create GET controller
 router.get('/', async (req, res) => {
   let user = req.user
-  console.log('hi there')
-  console.log(user)
-  res.render('houses/list', { user })
+
+  console.log(req.body)
+  console.log(req.query)
+
+  let q = req.query
+
+  if (q.location == 'Any Location') {
+    delete q.location
+  }
+
+  if (q.rooms == 'Any rooms') {
+    delete q.rooms
+  }
+
+  if (q.price != '') {
+    q.price = {
+      $lt: q.price
+    }
+  } else {
+    delete q.price
+  }
+
+  // if (q.title == '') {
+  // 	delete q.title
+  // }
+
+  req.query = q
+
+  let houses = await Houses.find(req.query).sort('-price')
+
+  res.render('houses/list', { houses, user })
 })
 
 router.get('/create', async (req, res) => {
@@ -35,31 +71,55 @@ router.get('/create', async (req, res) => {
   }
 })
 
-router.get('/:id', async (req, res) => {
-  res.render('houses/one')
+router.get('/:id', async (req, res, next) => {
+  try {
+    let house = await Houses.findById(req.params.id).populate('host')
+    res.render('houses/one', { house, user: req.user })
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.get('/:id/edit', async (req, res) => {
   if (req.isAuthenticated()) {
-    res.render('houses/edit', { user })
+    user = req.user
+
+    let house = await Houses.findById(req.params.id)
+    //console.log(house)
+
+    res.render('houses/edit', { user, house })
   } else {
-    res.redirect('/auth/login', { user })
+    res.redirect('/auth/login')
   }
 })
 
 // Create PATCH controller
-router.patch('/:id', async (req, res) => {
-  if (req.isAuthenticated()) {
-    res.render('houses/one')
-  } else {
-    res.redirect('/auth/login')
+router.patch('/:id', async (req, res, next) => {
+  console.log('hello edit house')
+  console.log(req.body)
+  try {
+    let houseId = req.params.id
+    // console.log(req.user)
+    // everything inside try
+    if (req.isAuthenticated()) {
+      let filteredPhotos = req.body.photos.filter(n => n != '')
+      req.body.photos = filteredPhotos
+
+      await Houses.findByIdAndUpdate(houseId, req.body)
+
+      res.redirect('/houses/' + houseId)
+    } else {
+      res.redirect('/auth/login')
+    }
+  } catch (err) {
+    next(err)
   }
 })
 
 // Create DELETE controller
 router.delete('/:id', async (req, res) => {
   if (req.isAuthenticated()) {
-    res.render('houses/one')
+    res.render('houses/one/')
   } else {
     res.redirect('/auth/login')
   }
